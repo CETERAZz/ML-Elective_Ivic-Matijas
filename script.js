8b65723483fd701cd8e4a94ba1c3ee532855e564
CETERAZz <45353182+CETERAZz@users.noreply.github.com>
2020-08-22 14:23:52 +0200

    Update
    Classification works now

--- script.js
+++ script.js
@@ -1,71 +1,27 @@
-// function draw() {
-//   image("images/oliver.jpg", 350, 350);
-// }
-// let video;
-function preload(){
-    img = loadImage("images/Aaron_Eckhart_0001.jpg");
+let model = "model/model.json";
+let classifier;
+let video;
+
+function setup() {
+  noCanvas();
+
+  video = createCapture(VIDEO);
+  video.style("display: flex; justify-self: center; border-radius: 5%;");
+
+
+  classifier = ml5.imageClassifier(model, video, modelReady);
 }
 
-  let nn;
-  // const IMAGE_WIDTH = 350;
-  // const IMAGE_HEIGHT = 350;
-  // const IMAGE_CHANNELS = 1;
+function modelReady() {
+  console.log("Model ready!");
+  classifier.predict(gotResult);
+}
 
-  // Step 2: initialize your neural network
-  function setup() {
-    createCanvas(350, 350);
-
-    const options = {
-      dataUrl: "data/legend.csv",
-      task: 'classification',
-      inputs:["image"],
-      outputs: ['emotion']
-    }
-
-    nn = ml5.neuralNetwork(options, dataLoaded);
+function gotResult(error, results) {
+  if (error) {
+    console.error();
+  } else {
+    document.getElementById("bestmatch").innerHTML = "It seems like you're " + results[0].label;
+    classifier.predict(gotResult);
   }
-
-  function draw(){
-    background(255);
-    image(img, 0, 0);
-  }
-
-  // Step 3: normalize data and train the model
-  function dataLoaded(){
-    nn.normalizeData();
-    trainModel();
-  }
-
-  // Step 4: train the model
-  function trainModel(){
-    const trainingOptions = {
-      epochs: 32,
-      batchSize: 12
-    }
-    nn.train(trainingOptions, finishedTraining);
-
-  }
-
-  // Step 5: use the trained model
-  function finishedTraining(){
-    classify();
-
-  }
-
-  // Step 6: make a classification
-  function classify(){
-    const input = {
-      image: "Aaron_Eckhart_0001.jpg"
-    };
-    nn.classify(input, handleResults);
-  }
-
-  // Step 7: define a function to handle the results of your classification
-  function handleResults(error, result) {
-      if(error){
-        console.error(error);
-        return;
-      }
-      console.log(results); // {label: 'red', confidence: 0.8};
-  }
-
+}
